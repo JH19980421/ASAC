1cc1503625902f67d34126e92c763541879d7fb8
JH19980421 <choijongho2@gmail.com>
2022-10-14 12:35:34 +0900

    modal

---
+++ week1/signup.js
@@ -0,0 +1,29 @@
+const modal = document.querySelector('.modal');
+const btnOpenPopup = document.querySelector('.btn_signup');
+const img = document.getElementsByTagName('img');
+const close = document.querySelector('.close');
+const data = document.querySelector('.NameModal');
+const nextBtn = document.querySelector('.signup_btn');
+
+btnOpenPopup.addEventListener('click', () => {
+    modal.style.visibility = 'visible';
+    data.style.visibility = 'hidden';
+    document.body.style = 'overflow: hidden';
+    document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    
+});
+
+nextBtn.addEventListener('click', () => {
+    data.style.visibility = 'hidden';
+    modal.style.visibility = 'visible';
+    document.body.style = 'overflow: hidden';
+    document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+});
+
+close.addEventListener('click', () => {
+    data.style.visibility = 'hidden';
+    modal.style.visibility = 'hidden';
+    document.body.style = 'block';
+    document.body.style.background = 'white';
+});
+
