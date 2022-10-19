f9dc3d5a3455e80ccc058742cd6b18b0570edaee
msdio <rlagofka1@naver.com>
2022-10-19 15:10:10 +0900

    [feat] input validations on join modals

--- week1/js/floating.js
+++ week1/js/floating.js
@@ -1,5 +1,9 @@
 const floatingContainer = document.querySelector('.floating');
 
+const likesContainer = document.querySelector('.likes');
+const likesImage = document.querySelector('.likes img');
+const likesCount = document.querySelector('.likes-count');
+
     
 window.onscroll = () => {
     if(window.scrollY > 2260) {
@@ -10,3 +14,11 @@ window.onscroll = () => {
         floatingContainer.style.top = '9%';
     }
 }
+
+let likeClicked = 0;
+likesContainer.addEventListener('click', () => {
+    likesImage.src = likeClicked? "./assets/images/heart.png": './assets/images/heart-filled.png';
+    likesCount.innerHTML = likeClicked? parseInt(likesCount.innerHTML)-1: parseInt(likesCount.innerHTML)+1;
+    likeClicked = likeClicked? 0: 1;
+});
+
--- week1/js/login-modal.js
+++ week1/js/login-modal.js
@@ -4,7 +4,14 @@ const modalContainer = document.querySelector('.login-modal');
 const joinModalContainer = document.querySelector('.login-modal-join');
 const closeModalButton = document.querySelectorAll('.login-modal-close');
 
+const emailInput = document.querySelector('.email-input');
+
 const gotoJoinModal = document.querySelector('.email-continue');
+const completeJoin = document.querySelector('.join-submit');
+
+const agreementAll = document.querySelector('.agreement input');
+const agreementItems = document.querySelectorAll('.agreement-item-check input');
+const agreementRequired = document.querySelector('.agreement-item-check:first-child input')
 
 
 loginButton.addEventListener('click', () => {
@@ -24,12 +31,50 @@ closeModalButton.forEach((item) => {
 });
 
 gotoJoinModal.addEventListener('click', () => {
-    joinModalContainer.style.display = 'block';
-    joinModalContainer.style.zIndex = 6;
+    if(emailValidation(emailInput.value)) {
+        joinModalContainer.style.display = 'block';
+        joinModalContainer.style.zIndex = 6;
+    } else {
+        alert('이메일을 제대로 입력하세여');
+    }
 });
+
+completeJoin.addEventListener('click', () => {
+    if(!agreementRequired.checked) {
+        alert('필수 항목 체크해얗마');
+    } else {
+        alert('회원가입완료');
+        backgroundHider.style.display = 'none';
+        modalContainer.style.display = 'none';
+        joinModalContainer.style.display = 'none';
+    }
+})
 
 backgroundHider.addEventListener('click', () => {
     modalContainer.style.display = 'none';
     joinModalContainer.style.display = 'none';
     backgroundHider.style.display = 'none';
+});
+
+
+emailValidation = (email) => {
+    const regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+    return email.match(regExp)? true: false;
+}
+
+
+let isAllChecked = 0;
+agreementAll.addEventListener('click', () => {
+    if(!isAllChecked) {
+        isAllChecked = 1;
+        agreementItems.forEach((item) => {
+            item.checked = true;
+        });
+    } else {
+        isAllChecked = 0;
+        agreementItems.forEach((item) => {
+            item.checked = false;
+        });
+    }
 });
