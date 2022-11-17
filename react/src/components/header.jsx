794c56b57b5fdb3f7b3ff7cfb345b1279df0d9f3
msdio <rlagofka1@naver.com>
2022-11-17 14:45:15 +0900

    hamburger menu

--- react/src/components/header.jsx
+++ react/src/components/header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
@@ -6,17 +7,28 @@ import '../css/common.css';
 import '../css/header.css';
 
 import LoginModal from "./modals/login-modal";
-import { useEffect, useMemo, useState } from "react";
 import SearchBar from "./search-bar";
 
+const expandMenus = require("../expand-menu.json");
+const menuDetails = require("../menu-details.json");
+let menuDetailLength = Math.ceil(menuDetails.length / 15);
+
+
 function Header() {
+    const [expandMenu, setExpandMenu] = useState(false);
+    const [expandExtraMenu, setExpandExtraMenu] = useState(false);
+    const [menuName, setMenuName] = useState("");
     const [openLoginModal, setOpenLoginModal] = useState(false);
     const [showSearchBar, setShowSearchBar] = useState(false);
     const [isLogin, setIsLogin] = useState(false);
 
     useEffect(() => {
         console.log(window.localStorage.getItem('id'));
-    }, [])
+    }, []);
+
+    // useMemo(() => {
+    //     setMenuName(menuName);
+    // }, [menuName]);
 
     const onClickJoinLogin = () => {
         openLoginModal = setOpenLoginModal(!openLoginModal);
@@ -31,6 +43,20 @@ function Header() {
         window.localStorage.clear();
     }
 
+    const onMouseEnterMenu = () => {
+        setExpandMenu(true);
+    }
+
+    const onMouseLeaveExpandedMenu = () => {
+        setExpandMenu(false);
+    }
+
+    const onMouseEnterExpandedMenu = (name) => {
+        setExpandExtraMenu(true);
+
+        setMenuName(name);
+    }
+
     return (
         <HeaderContainer>
             {
@@ -39,7 +65,52 @@ function Header() {
             {
                 showSearchBar? <SearchBar showSearchBar={setShowSearchBar}/>: null
             }
-            <img className="menu" src={ require('../assets/images/menu.png') } alt="menu"/>
+            <MenuImage
+                expandMenu={expandMenu}
+                setExpandMenu={setExpandMenu}
+                onMouseEnter={onMouseEnterMenu}
+                src={ require('../assets/images/menu.png') } 
+                alt="menu"
+            />
+            {
+                expandMenu?
+                <ExpandedMenu onMouseLeave={onMouseLeaveExpandedMenu}>
+                    <ul>
+                        <li>직군 전체</li>
+                        {
+                            expandMenus.map((menu) => (
+                                <li 
+                                    key={menu.id}
+                                    onMouseOver={() => onMouseEnterExpandedMenu(menu.content)}
+                                    onMouseLeave={() => setExpandExtraMenu(false)}
+                                >
+                                    {menu.content}
+                                </li>
+                            ))
+                        }
+                        <Freelancer>프리랜서</Freelancer>
+                    </ul>
+                </ExpandedMenu>
+                :
+                null
+            }
+            {
+                expandExtraMenu?
+                <ExtraMenuContainer
+                    onMouseOver={() => {setExpandExtraMenu(true); setExpandMenu(true)}}
+                    onMouseLeave={() => setExpandExtraMenu(false)}
+                >
+                    {
+                        menuDetails.map((it) => (
+                            <p key={it.id}>{menuName}{it.id}</p>
+                        ))
+                    }
+                </ExtraMenuContainer>
+                :
+                null                
+            }
+            
+
             <Link to="..">
                 <img className="logo" src="https://www.wantedlab.com/img/logo.png" alt="wanted"/>
             </Link>
@@ -60,7 +131,7 @@ function Header() {
                 </li>
             </Category>
 
-            <div className="header__right">
+            <HeaderRight>
                 <img 
                     className="search" 
                     src={ require('../assets/images/search.png') } 
@@ -92,11 +163,10 @@ function Header() {
                 <p className="divider__vertical"></p>
         
                 {/* <div className="header__organization">기업 서비스</div>*/}
-                <div 
-                    className="header__organization"
-                    onClick={onClickLogout}
-                >로그아웃</div>
-            </div>
+                <HeaderOrganization onClick={onClickLogout}>
+                    로그아웃
+                </HeaderOrganization>
+            </HeaderRight>
         </HeaderContainer>
     );
 }
@@ -122,6 +192,18 @@ const HeaderContainer = styled.div`
     height: 50px;
 `;
 
+const HeaderRight = styled.div`
+    display: contents;
+    text-align: right;
+`;
+
+const MenuImage = styled.img`
+    width: 18px;
+    height: 19px;
+
+    cursor: pointer;
+`;
+
 const Category = styled.ul`
     display: flex;
     flex-grow: 1;
@@ -142,7 +224,6 @@ const Category = styled.ul`
 
         position: relative;
 
-        /* float: left; */
         width: max-content;
         height: 100%;
 
@@ -167,6 +248,106 @@ const BadgeNew = styled.em`
     color: blue;
 
     transform: translateX(10px) scale(0.6);
-`
+`;
+
+const HeaderOrganization = styled.div`
+    border: 1px solid lightgray;
+    border-radius: 20px;
+
+    padding: 5px 10px;
+    
+    color: gray;
+
+    cursor: pointer;
+`;
+
+const ExpandedMenu = styled.div`
+    width: 210px;
+    height: 93.4%;
+
+    overflow: scroll;
+
+    position: fixed;
+    left: 14.2%;
+    top: 6.6%;
+    z-index: 2;
+
+    background-color: white;
+
+    border-radius: 2px;
+
+    text-align: left;
+
+    ul {
+        list-style: none;
+
+        margin: 0;
+        line-height: 270%;
+        
+        font-size: 14px;
+        font-weight: bold;
+
+        padding: 0;
+        
+        li {
+            padding: 0 17px;
+
+            cursor: pointer;
+            
+            &:hover {
+                color: blue;
+                background-color: #f2f2f2;
+            }
+        }
+    }
+`;
+
+const Freelancer = styled.div`
+    width: inherit;
+
+    margin: 8px 17px;
+    padding: 9px 0;
+
+    background-color: lightblue;
+
+    text-align: center;
+
+    border-radius: 7px;
+`;
+
+const ExtraMenuContainer = styled.div`
+    position: fixed;
+    left: 28%;
+    top: 6.6%;
+    z-index: 2;
+
+    width: max-content;
+    height: 93.4%;
+
+    display: grid;
+    grid-template-columns: repeat(${menuDetailLength}, 180px);
+    grid-template-rows: repeat(17, 40px);
+    grid-auto-flow: column;
+
+    align-items: center;
+
+    background-color: #eff2f4;
+
+    /* row-gap: 30px; */
+
+    p {
+        width: fit-content;
+        height: fit-content;
+
+        margin: 5px 10px;
+
+        cursor: pointer;
+    }
+
+    p:hover {
+        color: blue;
+    }
+`;
+
 
 export default Header;
