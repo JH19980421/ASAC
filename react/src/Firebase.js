6208c93eea56ac432e48397252d53288156a1db9
msdio <rlagofka1@naver.com>
2022-12-02 10:34:34 +0900

    before migrate to typescript

--- react/src/Firebase.js
+++ react/src/Firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI3ti2gSMugbHCcWa2EK04MfssLWIfBCM",
-  authDomain: "upload-resume-e4ddb.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   // The value of `databaseURL` depends on the location of the database
   databaseURL: "https://DATABASE_NAME.firebaseio.com",
-  projectId: "upload-resume-e4ddb",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: "upload-resume-e4ddb.appspot.com",
   messagingSenderId: "SENDER_ID",
   appId: "APP_ID",
